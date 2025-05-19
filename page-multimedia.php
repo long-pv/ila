c1a6cb5cfbe25e7cce3e7655f91932fb2e2868fc
longpv <long05062000@gmail.com>
2025-05-20 00:20:02 +0700

    no message

--- page-multimedia.php
+++ page-multimedia.php
@@ -24,184 +24,160 @@ get_header();
                 <div class="container">
                     <div class="list-cate">
                         <ul>
+
                             <li class="navbar-blog-item">
-                                <a href="https://ila.edu.vn/lam-ban-cung-con">Làm bạn cùng con</a>
+                                <a href="https://iladuhoc.edu.vn/hoi-thao-su-kien">Hội thảo sự kiện</a>
                             </li>
 
                             <li class="navbar-blog-item">
-                                <a href="https://ila.edu.vn/bi-kip-hoc-tieng-anh">Bí kíp học tiếng Anh</a>
+                                <a href="https://iladuhoc.edu.vn/tin-tuc">Tin tức</a>
                             </li>
 
                             <li class="navbar-blog-item">
-                                <a href="https://ila.edu.vn/guong-mat-xuat-sac">Gương mặt xuất sắc</a>
+                                <a href="https://iladuhoc.edu.vn/tips-du-hoc">Tips du học</a>
                             </li>
 
                             <li class="navbar-blog-item">
-                                <a href="https://ila.edu.vn/tin-tuc-su-kien">Tin tức sự kiện</a>
+                                <a href="https://iladuhoc.edu.vn/xu-huong-nghe-nghiep-du-hoc">Xu hướng nghề nghiệp &amp; du học</a>
                             </li>
 
                             <li class="navbar-blog-item">
-                                <a href="https://ila.edu.vn/video">Multimedia</a>
+                                <a href="https://iladuhoc.edu.vn/video">Multimedia</a>
                             </li>
                         </ul>
                     </div>
                 </div>
             </section>
-            <!--  -->
-            <?php
-            // 1. Xác định trang hiện tại
-            $paged = get_query_var('paged') ? absint(get_query_var('paged')) : 1;
 
-            // 2. Thiết lập WP_Query
-            $args = [
-                'post_type' => 'video_media',
-                'posts_per_page' => 9,      // tổng số posts mỗi trang (1 đầu + 8 dưới)
-                'paged' => $paged,
-            ];
-            $video_query = new WP_Query($args);
 
-            if ($video_query->have_posts()): ?>
+            <section class="video-main">
+                <div class="container">
+                    <div class="row">
+                        <header class="page-header w-100">
+                            <h1 class="page-title">Multimedia</h1>
+                        </header>
+                    </div>
 
-                <section class="video-main">
-                    <div class="container">
+                    <?php
+                    $args = array(
+                        'post_type' => 'video_media',
+                        'posts_per_page' => -1
+                    );
+                    $query = new WP_Query($args);
 
-                        <div class="row">
-                            <header class="page-header">
-                                <h1 class="page-title">MULTIMEDIA</h1>
-                            </header>
-                        </div>
+                    if ($query->have_posts()) :
+                        $first = true;
+                        while ($query->have_posts()) : $query->the_post();
 
-                        <?php
-                        // --- 1) Video đầu tiên ---
-                        $video_query->the_post();
-                        // Lấy iframe từ ACF field "link_video"
-                        $iframe = get_field('link_video');
+                            $video_url = get_field('link_video');
+                            $video_duration = get_field('video_duration');
+                            $video_author = get_field('author');
+                            $thumbnail = get_the_post_thumbnail_url(get_the_ID(), 'full');
+                            $permalink = get_permalink();
+                            $title = get_the_title();
+                            $excerpt = get_the_excerpt();
+                            $published = get_the_date('l, d-m-Y, H:i');
+                            $category_link = get_post_type_archive_link('video_media');
+                    ?>
 
-                        $title = get_the_title();
-                        $excerpt = get_the_excerpt();
-                        $author = get_the_author();
-                        $date = get_the_date('l, d-m-Y, H:i (G\MT+7)');
-                        ?>
-
-                        <div class="video-first-play">
-                            <div class="row">
-                                <div class="col-lg-8">
-                                    <div class="video-iframe">
-                                        <?php
-                                        // In thẳng iframe được lưu trong field (đã tin tưởng)
-                                        echo $iframe;
-                                        ?>
-                                    </div>
-                                </div>
-                                <div class="col-lg-4">
-                                    <div class="video-content">
-                                        <div class="video-title">
-                                            <h2><a href="<?php the_permalink(); ?>"><?php echo esc_html($title); ?></a>
-                                            </h2>
+                            <?php if ($first) : $first = false; ?>
+                                <div class="video-first-play">
+                                    <div class="row">
+                                        <div class="col-sm-12 col-lg-8 col-xl-8">
+                                            <div class="video-iframe">
+                                                <iframe id="player-yt" data-url="https://www.youtube.com/watch?v=9MO7Qn9BEEA" frameborder="0" allowfullscreen="" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" title="Ngày hội du học ILA 2025: Cơ hội thực hiện hóa giấc mơ du học" width="640" height="360" src="https://www.youtube.com/embed/9MO7Qn9BEEA?autoplay=0&amp;controls=1&amp;mute=1&amp;rel=0&amp;showinfo=0&amp;enablejsapi=1&amp;origin=https%3A%2F%2Filaduhoc.edu.vn&amp;widgetid=1&amp;forigin=https%3A%2F%2Filaduhoc.edu.vn%2Fvideo&amp;aoriginsup=1&amp;gporigin=https%3A%2F%2Filaduhoc.edu.vn%2Fvideo&amp;vf=6" data-gtm-yt-inspected-9="true"></iframe>
+                                            </div>
                                         </div>
-                                        <div class="video-content_desc">
-                                            <p><?php echo esc_html($excerpt); ?></p>
-                                        </div>
-                                        <div class="video-author">
-                                            <p><?php echo esc_html($author); ?></p>
-                                        </div>
-                                        <div class="video-time-publish">
-                                            <p>
-                                                <a
-                                                    href="<?php echo esc_url(get_post_type_archive_link('video_media')); ?>">MULTIMEDIA</a>
-                                                <span><?php echo esc_html($date); ?></span>
-                                            </p>
-                                        </div>
-                                        <div class="video-share-social">
-                                            <?php if (function_exists('a2a_share_save_widget')) {
-                                                // nếu bạn dùng AddToAny
-                                                a2a_share_save_widget();
-                                            } ?>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <?php
-                        // --- 2) Các video còn lại ---
-                        if ($video_query->have_posts()): ?>
-                            <div class="row">
-                                <?php
-                                $col_count = 0;
-                                while ($video_query->have_posts()):
-                                    $video_query->the_post();
-                                    $thumb_url = get_the_post_thumbnail_url(get_the_ID(), 'full');
-                                    $vid_title = get_the_title();
-                                    $permalink = get_permalink();
-                                    // duration lưu trong custom field "duration" nếu có
-                                    $duration = get_field('duration') ?: '';
-                                    ?>
-                                    <div class="video-list col-6 col-sm-3 col-md-4 col-lg-3">
-                                        <article class="video-item">
-                                            <?php if ($thumb_url): ?>
-                                                <div class="video-thumb">
-                                                    <a href="<?php echo esc_url($permalink); ?>">
-                                                        <img src="<?php echo esc_url($thumb_url); ?>"
-                                                            alt="<?php echo esc_attr($vid_title); ?>" loading="lazy">
-                                                    </a>
+                                        <div class="col-sm-12 col-lg-4 col-xl-4">
+                                            <div class="video-content">
+                                                <div class="video-title">
+                                                    <h2><a href="<?php echo esc_url($permalink); ?>" target="_blank"><?php echo esc_html($title); ?></a></h2>
                                                 </div>
-                                            <?php endif; ?>
+                                                <div class="video-content_desc">
+                                                    <?php the_content(); ?>
+                                                </div>
+                                                <div class="video-author">
+                                                    <p><?php echo esc_html($video_author); ?></p>
+                                                </div>
+                                                <div class="video-time-publish">
+                                                    <p><a href="<?php echo esc_url($category_link); ?>">Multimedia</a><span><?php echo esc_html($published); ?> (GTM+7)</span></p>
+                                                </div>
+                                                <div class="video-share-social">
+                                                    <section id="a2a_share_save_widget-2" class="widget widget_a2a_share_save_widget">
+                                                        <div class="a2a_kit a2a_kit_size_20 addtoany_list" style="line-height: 20px;">
+                                                            <a class="a2a_button_facebook" href="/#facebook" title="Facebook" rel="nofollow noopener" target="_blank"><span class="a2a_label">Facebook</span></a>
+                                                            <a class="a2a_button_twitter" href="/#twitter" title="Twitter" rel="nofollow noopener" target="_blank"><span class="a2a_label">Twitter</span></a>
+                                                            <a class="a2a_button_copy_link" href="/#copy_link" title="Copy Link" rel="nofollow noopener" target="_blank"><span class="a2a_label">Copy Link</span></a>
+                                                            <a class="a2a_dd addtoany_share_save addtoany_share" href="https://www.addtoany.com/share"><span class="a2a_label a2a_localize" data-a2a-localize="inner,Share">Chia sẻ</span></a>
+                                                        </div>
+                                                    </section>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                <?php else : ?>
+                                    <div class="video-list col-6 col-md-4 col-lg-3">
+                                        <div class="video-item">
+                                            <div class="video-thumb">
+                                                <a href="<?php echo esc_url($video_url); ?>" target="_blank">
+                                                    <img src="<?php echo esc_url($thumbnail); ?>" alt="<?php echo esc_attr($title); ?>">
+                                                </a>
+                                            </div>
                                             <div class="video-title">
                                                 <h3>
-                                                    <a href="<?php echo esc_url($permalink); ?>">
-                                                        <?php echo esc_html($vid_title); ?>
-                                                    </a>
+                                                    <a href="<?php echo esc_url($video_url); ?>" target="_blank"><?php echo esc_html($title); ?></a>
                                                 </h3>
                                             </div>
                                             <div class="video-time-cate">
                                                 <p>
-                                                    <?php if ($duration): ?>
-                                                        <span class="icon-time">
-                                                            <i class="fa-solid fa-timer"></i>
-                                                            <?php echo esc_html($duration); ?>
-                                                        </span>
-                                                    <?php endif; ?>
-                                                    <span class="category-link">
-                                                        <a href="<?php echo esc_url(get_post_type_archive_link('video_media')); ?>">
-                                                            MULTIMEDIA
-                                                        </a>
-                                                    </span>
+                                                    <span class="icon-time"><i class="fa-solid fa-timer"></i> <?php echo esc_html($video_duration); ?></span>
+                                                    <span class="category-link"><a href="<?php echo esc_url($category_link); ?>">Multimedia</a></span>
                                                 </p>
                                             </div>
-                                        </article>
+                                        </div>
                                     </div>
-                                    <?php
-                                endwhile;
-                                ?>
-                            </div>
-                        <?php endif; // end if have_posts() for remaining ?>
+                                <?php endif; ?>
 
-                        <?php
-                        // --- 3) Phân trang ---
-                        echo '<div class="panigation-archive text-center">';
-                        echo paginate_links([
-                            'total' => $video_query->max_num_pages,
-                            'current' => $paged,
-                            'mid_size' => 1,
-                            'prev_text' => '&laquo;',
-                            'next_text' => '&raquo;',
-                        ]);
-                        echo '</div>';
+                            <?php endwhile; ?>
+                                </div> <!-- end .row -->
+                            <?php endif;
+                        wp_reset_postdata(); ?>
+                </div>
+            </section>
 
-                        // Reset lại postdata
-                        wp_reset_postdata();
-                        ?>
 
-                    </div><!-- .container -->
-                </section>
 
-            <?php endif; // end if main query have_posts() ?>
-        </div>
-        <!-- .wrap -->
+        </div><!-- .wrap -->
     </main>
 </div>
 <!-- #content -->
+
+
+<style>
+    /* Responsive iframe container */
+    .video-iframe {
+        position: relative;
+        padding-bottom: 56.25%;
+        /* 16:9 aspect ratio */
+        height: 0;
+        overflow: hidden;
+        max-width: 100%;
+        background: #000;
+    }
+
+    /* Responsive iframe */
+    .video-iframe iframe {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        border: 0;
+    }
+</style>
 
 <?php
 get_footer();
